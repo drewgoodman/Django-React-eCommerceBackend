e9c008755bee7c16d26a4924744ab4be318df7ab
rdogm13 <drg6525@yahoo.com>
2021-04-12 17:40:03 -0700

    refactored urls and views into separate scripts

---
+++ base/views/product_views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import status
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product
+from base.serializers import ProductSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/delete/<id>/',
+        '/api/products/update/<id>/'
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
@@ -10,10 +9,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.models import User
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
-# Create your views here.
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
    def validate(self, attrs):
@@ -56,31 +52,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/update/<id>/'
-    ]
-    return Response(routes)
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)

9121701c9f49f2d8bb6b17b0f40e9fb0e27ee1c3
rdogm13 <drg6525@yahoo.com>
2021-08-07 15:52:18 -0700

    list all of users orders view

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 
 from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
+from datetime import datetime
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -62,6 +63,14 @@ def addOrderItems(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
     user = request.user
     try:
@@ -73,3 +82,13 @@ def getOrderById(request, pk):
             return Response({'detail': 'Not authorized to view this order.'}, status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response("Order was paid")

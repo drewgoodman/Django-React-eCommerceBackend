aa22c94efc3cc6b782894d463026a14e2b986485
rdogm13 <drg6525@yahoo.com>
2021-08-08 21:00:51 -0700

    delete user admin function

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register/', views.registerUser, name="user-register"),
     path('profile/', views.getUserProfile, name="users-profile"),
     path('profile/update/', views.updateUserProfile, name="users-update-profile"),
-    path('', views.getUsers, name="users")
+    path('', views.getUsers, name="users"),
+    path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
 ]

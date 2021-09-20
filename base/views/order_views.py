41cad972f2897d3e557fbea8689522742e32ffbe
rdogm13 <drg6525@yahoo.com>
2021-09-19 18:33:13 -0700

    list all orders view

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -92,3 +92,10 @@ def updateOrderToPaid(request, pk):
     order.paidAt = datetime.now()
     order.save()
     return Response("Order was paid")
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
